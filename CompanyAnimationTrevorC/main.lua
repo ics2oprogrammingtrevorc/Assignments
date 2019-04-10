1342a11eb5d4403319dab256e55c74634add8a94
Trevor Cosby <47821639+s448315@users.noreply.github.com>
2019-04-10 09:45:31 -0400

    assignment 6

--- CompanyAnimationTrevorC/main.lua
+++ CompanyAnimationTrevorC/main.lua
@@ -5,3 +5,41 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+display.setStatusBar(display.HiddenStatusBar)
+
+scrollSpeed = 3
+
+-- display company logo
+local paw = display.newImageRect("Images/CompanyLogo.png", 70, 70)
+paw.x = 100
+paw.y = display.contentHeight/1
+
+-- make it invisible
+paw.alpha = 0
+
+
+function stopPaw()
+	Runtime:removeEventListener("enterFrame", movePaw)
+	paw.x = display.contentCenterX
+	paw.y = display.contentCenterY
+end
+
+function movePaw(event)
+	paw.x = paw.x + scrollSpeed
+	paw.y = paw.y - scrollSpeed
+	-- fade in
+	paw.alpha = paw.alpha + 0.02
+	-- rotates
+	paw:rotate(20.86)
+	-- Makes it bigger
+	paw.xScale = paw.xScale + 0.05
+	paw.yScale = paw.yScale + 0.05
+	if (paw.x >= display.contentCenterX) then
+		stopPaw()
+	end
+end
+
+
+
+Runtime:addEventListener("enterFrame", movePaw)
